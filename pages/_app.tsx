68eed2c26b030ca3b586ce2ead6d2b62fe73c83d
Manassarn Manoonchai <manassarn@gmail.com>
2020-07-22 19:41:58 +0700

    :heavy_plus_sign: Setup Tailwindcss

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from 'next/app'
+
+import '../styles/index.css'
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default App
---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: [
+              './components/**/*.{js,jsx,ts,tsx}',
+              './pages/**/*.{js,jsx,ts,tsx}'
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        }
+      : {})
+  }
+};
+
